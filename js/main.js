8448ab59ce5c45700aa16708b219e56a20590c91
terryopp <61347225+terryopp@users.noreply.github.com>
2020-09-24 20:09:07 +0500

    realisation of change types of encrypting

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
 window.onload = function() {
     let input = document.getElementById('text');
     let output = document.getElementById('output');
+    let type = document.getElementById('type');
 
     let encrypt = new Cipher();
 
+    let selectedType = true;
+
+    type.addEventListener("click", () => {
+        selectedType = !selectedType;
+        if (selectedType) {
+            type.value = 'Encrypt';
+        } else {
+            type.value = 'Decrypt';
+        }
+    });
+
+
     input.addEventListener("input", () => {
         let text = input.value;
-        output.value = encrypt.encrypt(text, 1);
-    });
+        if (selectedType) {
+            output.value = encrypt.encrypt(text, 1);
+        } else {
+            output.value = encrypt.decrypt(text, 1);
+        }
+    });  
 }
